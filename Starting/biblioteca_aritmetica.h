9b4f4ffd72851f1acb23ced15fb9dfad9dcccfde
gmrscarol <67639331+gmrscarol@users.noreply.github.com>
2020-09-13 18:29:31 -0300

    Update biblioteca_aritmetica.h

--- Starting/biblioteca_aritmetica.h
+++ Starting/biblioteca_aritmetica.h
@@ -1,3 +1,7 @@
+//Para criar uma biblioteca em C eh preciso criar dois arquivos, um arquivo cabecalho .h , 
+//que contem as declaracoes e definicoes do que esta contido dentro da biblioteca (definidos funcoes e tipos de variaveis que farao parte da biblioteca)
+// e um arquivo .c, chamado de codigo-fonte da biblioteca, que contem a implementacao das funcoes definidas no cabecalho.
+
 int soma (int a, int b);
 int subtracao(int a, int b);
 int produto( int a, int b);
