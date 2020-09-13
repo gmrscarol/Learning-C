40d05bc175385d2f2d0c39a080263b881571425a
carol <lucascpds@gmail.com>
2020-09-13 12:21:43 -0300

    primeiro commit

---
+++ Starting/main.c
@@ -0,0 +1,13 @@
+// Esqueleto de um programa
+
+#include <stdio.h> //Declaracoes globais: Bibliotecas usadas pelo programa
+#include <stdlib.h>
+
+
+
+int main() // Inicio do programa. Funcao main foi definida como funcao do tipo inteiro -> precisa devolver valor inteiro (return 0)
+{
+    printf("Hello world!\n"); //escreve uma mensagem na tela de saida
+    system("pause"); // faz uma pausa antes de come'car o programa
+    return 0; //Retorna o codigo de erro zero, informando que esta tudo OK
+} // FIm do programa
